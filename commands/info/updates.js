a9bb779f5f2c8ea1463e395b8feb406c6af5ddb6
lisa-wolfgang <43426138+lisa-wolfgang@users.noreply.github.com>
2024-08-27 01:01:25 -0500

    Fix v3.8 log

--- commands/info/updates.js
+++ commands/info/updates.js
@@ -15,7 +15,6 @@ module.exports = {
             name: "8/26/24 - v3.8",
             value: `
           - Increased \`pb wordsearch\` word bank from 251 to 401 words.
-          - Updated internally to no longer require replit.com to run the bot.
           `
           },
           {
